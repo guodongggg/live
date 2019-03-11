d0685b9666c4f87488f809d78e8e4d2d2851a0af
jelly <guodongggg@gmail.com>
2019-03-11 20:52:09 +0800

    熊猫TV倒闭....注释掉pandaTV相关代码

--- windows/views.py
+++ windows/views.py
@@ -12,11 +12,11 @@ from .online_sort import *
 def index(request):
     douyu_lol = Spider().douyu('lol')
     huya_lol = Spider().huya('lol')
-    panda_lol = Spider().panda('lol')
+    #panda_lol = Spider().panda('lol')
     qie_lol = Spider().qie('lol')
 
     # 合并多个平台
-    old_infos = douyu_lol + huya_lol + panda_lol + qie_lol
+    old_infos = douyu_lol + huya_lol + qie_lol
 
     # 将含万的人气值转换为数字，重新进行排序
     all_infos = OnlineSort().online_change(old_infos)
@@ -29,10 +29,12 @@ def common(request, item):
     douyu = Spider().douyu(item)
     huya = Spider().huya(item)
     qie = Spider().qie(item)
-    panda = Spider().panda(item)
+
+    #熊猫TV倒闭
+    #panda = Spider().panda(item)
 
     # 合并多个平台
-    old_infos = douyu + huya + qie + panda
+    old_infos = douyu + huya + qie
 
     # 将含万的人气值转换为数字，重新进行排序
     all_infos = OnlineSort().online_change(old_infos)
